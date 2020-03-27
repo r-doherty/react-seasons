8c3e57502cf7ec7483db5d594ce3e666e8f20263
Ryan Doherty <rjd.atx@gmail.com>
2020-03-27 15:02:07 -0500

    Spinner and Season cutomization function adds

--- src/index.js
+++ src/index.js
@@ -1,25 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
 
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = { lat: null, errorMessage: '' };
-    }
+    state = { lat: null, errorMessage:''};
 
     componentDidMount() {
         window.navigator.geolocation.getCurrentPosition(
-            (position) => this.setState({ lat: position.coords.latitude }),
-            (err) => this.setState({ errorMessage: err.message })
+            position => this.setState({ lat: position.coords.latitude }),
+            err => this.setState({ errorMessage: err.message })
         );
     }
 
     componentDidUpdate() {
     }
 
-    // React says we have to define render!
-    render() {
+    renderContent() {
         if(this.state.errorMessage && !this.state.lat)
         {
             return <div>Error: {this.state.errorMessage}</div>;
@@ -27,14 +25,16 @@ class App extends React.Component {
 
         if(!this.state.errorMessage && this.state.lat)
         {
-            return <div>Latitude: {this.state.lat}</div>;
+            return <SeasonDisplay lat={this.state.lat} />;
         }
 
-        if(!this.state.errorMessage && !this.state.lat)
-        {
-            return <div>Loading...!</div>;
-        }
+        return <Spinner message="Please accept location request" />;
     }
+
+    render() {
+        return(<div className="border red">{this.renderContent()}</div>);
+    };
+
 }
 
 
